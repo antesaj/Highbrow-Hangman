3434d9ea011266ef69882691856e2df9b3e7e389
Andrew Antes <antesaj@gmail.com>
2017-11-27 18:56:32 -0800

    Add files via upload

--- Highbrow Hangman/src/HangmanSound.java
+++ Highbrow Hangman/src/HangmanSound.java
@@ -1,4 +1,11 @@
 
+//*******************************************************
+// AUTHORS: Andrew Antes, Cameron Troy, 
+//          Steven Foust, and Gabrielle Dubbs
+// PROGRAM: HangmanSound.java
+// DESCRIPTION: Creates new a new Media and MediaPlayer.
+// 				This adds the sound to the Hangman game.
+//*******************************************************
 import java.io.File;
 
 import javafx.scene.media.Media;
@@ -9,26 +16,26 @@ public class HangmanSound {
 	private String soundFile;
 	private Media inGameSound;
 	private MediaPlayer gameSoundPlayer;
-	
+
 	public HangmanSound(String pFile) {
 		soundFile = pFile;
-	}
-	
+	} // End of ctor() block
+
 	public void play() {
 		inGameSound = new Media(new File(soundFile).toURI().toString());
 		gameSoundPlayer = new MediaPlayer(inGameSound);
 		gameSoundPlayer.setCycleCount(MediaPlayer.INDEFINITE);
 		gameSoundPlayer.play();
-	}
-	
+	} // End of method block
+
 	public void playOnce() {
 		inGameSound = new Media(new File(soundFile).toURI().toString());
 		gameSoundPlayer = new MediaPlayer(inGameSound);
 		gameSoundPlayer.play();
-	}
-	
+	}// End of method block
+
 	public void stop() {
 		gameSoundPlayer.stop();
-	}
-	
-}
+	} // End of method block
+
+} // End of class block
